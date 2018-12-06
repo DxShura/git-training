7ef00e3c8d9589489c24c1a8f85fa19bd73d7866
DxShura <sachau888@hotmail.com>
2018-12-06 11:30:50 +0100

    add info

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('test211155555');
+
+/**
+ * gco -d nomdebranche
+ * gco nom de branche pour switch
+ * 
+ * dans vscode faire ctrl+maj+p et chercher git checkout pour switch de branch
+ * 
+ * faire des changement dans la branche 
+ * Pour push le changement (faire add commit dans la branche) de la branche sur github, revenir sur la master et et faire git push origin develop
+ * 
+ * pour merge aller sur master (ou develop) et faire git merge develop (ou inverse)
+ */
